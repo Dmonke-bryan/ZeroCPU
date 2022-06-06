6d4447dbc4a906a459582615892f720ed14de770
tracer-oscpu2021 <tracer@oscpu.org>
2022-06-06 15:24:42 +0800

    Zhou Zhi Dao 20210407    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9f996dd697b34a36896fae9c1cbc99a6            Boot ID: cc490ddb67174b8fab060c0d1d23bfff     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-44-generic       Architecture: x86-64  15:24:42 up  3:34,  1 user,  load average: 0.71, 0.59, 0.41

---
+++ Difftest_Platform/oscpu-framework/difftest_example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#simulation 
+#./build.sh -e zerocpu -b -t zerocore.v -s
+
+#diff test - addi
+#./build.sh -e zerocpu -d -b -s -a "-i inst_diff.bin"
+./build.sh -e cpu_diff -d -b -s -a "-i inst_diff.bin"
--- Difftest_Platform/oscpu-framework/projects/cpu_diff/build/emu-compile/difftest.d
+++ Difftest_Platform/oscpu-framework/projects/cpu_diff/build/emu-compile/difftest.d
@@ -1,10 +1,10 @@
 difftest.o: \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/difftest/difftest.cpp \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/difftest/difftest.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/common/common.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/common/../../../../config/config.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/difftest/nemuproxy.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/difftest/goldenmem.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/common/ram.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/common/common.h \
- /home/bryan/ysyx/oscpu-framework/libraries/difftest/src/test/csrc/difftest/spikedasm.h
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/difftest/difftest.cpp \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/difftest/difftest.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/common/common.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/common/../../../../config/config.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/difftest/nemuproxy.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/difftest/goldenmem.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/common/ram.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/common/common.h \
+ /home/bryan/ZeroCPU/Difftest_Platform/oscpu-framework/libraries/difftest/src/test/csrc/difftest/spikedasm.h
