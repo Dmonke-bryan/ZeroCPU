eed6deb8d951c8985a4ce926c563e14f8455fe4c
tracer-oscpu2021 <tracer@oscpu.org>
2022-06-06 15:04:45 +0800

    Zhou Zhi Dao 20210407    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9f996dd697b34a36896fae9c1cbc99a6            Boot ID: cc490ddb67174b8fab060c0d1d23bfff     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-44-generic       Architecture: x86-64  15:04:45 up  3:14,  1 user,  load average: 0.44, 0.36, 0.30

--- Difftest_Platform/oscpu-framework/difftest_zerocpu.sh
+++ Difftest_Platform/oscpu-framework/difftest_zerocpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #simulation 
-./build.sh -e zerocpu -b -t zerocore.v -s
+#./build.sh -e zerocpu -b -t zerocore.v -s
 
-#./build.sh -e zerocpu -d -b -s -a "-i add-riscv-tests.bin"
+#diff test - addi
+./build.sh -e zerocpu -d -b -s -a "-i inst_diff.bin"
 #./build.sh -e cpu_diff -d -b -s -a "-i inst_diff.bin"
